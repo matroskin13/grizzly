cf15e379b08aa1728fab017c583632a11165e4ff
valentin.dubrovskiy <v.dubrovskiy@rambler-co.ru>
2017-06-11 20:07:39 +0300

    add new methods

--- collection/collection.go
+++ collection/collection.go
@@ -57,3 +57,37 @@ func (c *Collection) MapToString(callback func(item *Model) string) []string {
 
 	return items
 }
+
+func (c *Collection) Get(index int) (model *Model) {
+	if len(c.Items) > index {
+		return c.Items[index]
+	}
+
+	return model
+}
+
+func (c *Collection) Push(item *Model) *Collection {
+	newItems := append(c.Items, item)
+
+	return &Collection{Items: newItems}
+}
+
+func (c *Collection) Shift() *Model {
+	item := c.Items[0];
+	c.Items = c.Items[1:];
+
+	return item
+}
+
+func (c *Collection) Pop() *Model {
+	item := c.Items[len(c.Items) - 1];
+	c.Items = c.Items[:len(c.Items) - 1];
+
+	return item
+}
+
+func (c *Collection) Unshift(item *Model) *Collection {
+	newItems := append([]*Model{item}, c.Items...)
+
+	return &Collection{Items: newItems}
+}
--- collection/collection_test.go
+++ collection/collection_test.go
@@ -57,3 +57,45 @@ func TestCollection_MapToString(t *testing.T) {
 
 	assert.Equal(t, items, []string{"item1", "item2", "item3", "item4"})
 }
+
+func TestCollection_Get(t *testing.T) {
+	item := GetCollection().Get(1)
+
+	assert.Equal(t, item.GrizzlyId, 2)
+
+	item = GetCollection().Get(20)
+
+	assert.Nil(t, item)
+}
+
+func TestCollection_Push(t *testing.T) {
+	collection := GetCollection().Push(&Model{GrizzlyId: 10, GrizzlyName: "test"})
+
+	assert.Len(t, collection.Items, 5)
+	assert.Equal(t, collection.Get(4).GrizzlyId, 10)
+}
+
+func TestCollection_Pop(t *testing.T) {
+	collection := GetCollection()
+	item := collection.Pop()
+
+	assert.Equal(t, item.GrizzlyId, 4)
+	assert.Len(t, collection.Items, 3)
+	assert.Equal(t, collection.Get(2).GrizzlyId, 3)
+}
+
+func TestCollection_Unshift(t *testing.T) {
+	collection := GetCollection().Unshift(&Model{GrizzlyId: 10, GrizzlyName: "test"})
+
+	assert.Len(t, collection.Items, 5)
+	assert.Equal(t, collection.Get(0).GrizzlyId, 10)
+}
+
+func TestCollection_Shift(t *testing.T) {
+	collection := GetCollection()
+	item := collection.Shift()
+
+	assert.Equal(t, item.GrizzlyId, 1)
+	assert.Len(t, collection.Items, 3)
+	assert.Equal(t, collection.Get(2).GrizzlyId, 4)
+}
